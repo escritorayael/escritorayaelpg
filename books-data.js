638e12a7f48c5cfdb506e7b1cdb5cc387b19f2b7
escritora <goldmannyael@gmail.com>
2026-01-05 23:50:03 -0400

    Add files via upload

---
+++ books-data.js
@@ -0,0 +1,93 @@
+// books-data.js
+// Lista única de libros para el buscador (título, fecha, cover, link, etc.)
+window.BOOKS = [
+  
+  {
+    title: "Amina, la princesa rebelde",
+    date: "2025-01-26",
+    categories: ["Amor", "Novela"],
+    cover: "IMAGES/amina book.png",
+    url: "book-details.html?book=libro-1",
+    comments: 8,
+    popularity: 88
+  },
+  
+    {
+    title: "Cuando Todo Calla",
+    date: "2025-05-31",
+    categories: ["Amor", "Novela"],
+    cover: "IMAGES/cuando todo calla book.png",
+    url: "book-details.html?book=libro-2",
+    comments: 12,
+    popularity: 95
+  },
+  
+
+  {
+    title: "Destinos Entrelazados",
+    date: "2024-01-23",
+    categories: ["Amor", "Novela"],
+    cover: "IMAGES/destinos entrelazados.png",
+    url: "book-details.html?book=libro-3",
+    comments: 12,
+    popularity: 95
+  },
+  {
+    title: "En el Nombre de Ella",
+    date: "2025-04-27",
+    categories: ["Amor", "Misterio"],
+    cover: "IMAGES/en el nombre de ella book.png",
+    url: "book-details.html?book=libro-4",
+    comments: 8,
+    popularity: 88
+  },
+  {
+    title: "Encuentros Casuales",
+    date: "2024-05-19",
+    categories: ["Amor", "Suspenso"],
+    cover: "IMAGES/encuentros casuales book.png",
+    url: "book-details.html?book=libro-5",
+    comments: 3,
+    popularity: 70
+  },
+
+  {
+    title: "identidades-ocultas",
+    date: "2024-08-14",
+    categories: ["Amor", "Suspenso"],
+    cover: "IMAGES/identidades ocultas book.png",
+    url: "book-details.html?book=libro-6",
+    comments: 8,
+    popularity: 88
+  },
+  {
+    title: "Jaque a la Reina: Códigos Mortales",
+    date: "2024-02-26",
+    categories: ["Pasión", "Poder"],
+    cover: "IMAGES/jaque reina book.png",
+    url: "book-details.html?book=libro-7",
+    comments: 3,
+    popularity: 70
+  },
+
+    {
+    title: "Cómo sobrevivir a la vida",
+    date: "2024-04-02",
+    categories: ["Motivación", "Autoayuda"],
+    cover: "IMAGES/sobrevivir vida book.png",
+    url: "book-details.html?book=libro-8",
+    comments: 3,
+    popularity: 70
+  },
+
+    {
+    title: "Hasta que la Vida me Encuentre",
+    date: "2025-11-05",
+    categories: ["Amor", "Pasión"],
+      cover: "IMAGES/vida-encuentre-book.png",
+    url: "book-details.html?book=libro-9",
+    comments: 3,
+    popularity: 70
+  }
+
+];
